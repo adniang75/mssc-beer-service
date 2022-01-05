1121225d9d163fbd7c77cdf10c3a8217d82899fa
Alassane NIANG <adniang75@gmail.com>
2022-01-05 17:29:22 +0100

    Documenting Requests. closes #19

--- src/test/java/com/alassaneniang/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/alassaneniang/msscbeerservice/web/controller/BeerControllerTest.java
@@ -24,8 +24,7 @@ import static org.mockito.BDDMockito.given;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -74,7 +73,7 @@ class BeerControllerTest {
                                         fieldWithPath( "createdDate" ).description( "Date created" ),
                                         fieldWithPath( "lastModifiedDate" ).description( "Date updated" ),
                                         fieldWithPath( "beerName" ).description( "Beer name" ),
-                                        fieldWithPath( "beerStyle" ).description( "Ber style" ),
+                                        fieldWithPath( "beerStyle" ).description( "Beer style" ),
                                         fieldWithPath( "upc" ).description( "UPC of beer" ),
                                         fieldWithPath( "price" ).description( "Beer price" ),
                                         fieldWithPath( "quantityOnHand" ).description( "Quantity on hand" )
@@ -93,7 +92,23 @@ class BeerControllerTest {
                                 .contentType( APPLICATION_JSON )
                                 .content( beerDTOJSON )
                 )
-                .andExpect( status().isCreated() );
+                .andExpect( status().isCreated() )
+                .andDo(
+                        document(
+                                "v1/beer",
+                                requestFields(
+                                        fieldWithPath( "id" ).ignored(),
+                                        fieldWithPath( "version" ).ignored(),
+                                        fieldWithPath( "createdDate" ).ignored(),
+                                        fieldWithPath( "lastModifiedDate" ).ignored(),
+                                        fieldWithPath( "beerName" ).description( "Beer name" ),
+                                        fieldWithPath( "beerStyle" ).description( "Beer style" ),
+                                        fieldWithPath( "upc" ).description( "UPC of beer" ).attributes(),
+                                        fieldWithPath( "price" ).description( "Beer price" ),
+                                        fieldWithPath( "quantityOnHand" ).ignored()
+                                )
+                        )
+                );
     }
 
     @Test
